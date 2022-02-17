31a51a9c88b369cf9d6f5683a20b68d71bfdc29e
Ivan Monteiro Cantalice <ivanmonteiroc@gmail.com>
2022-02-17 16:15:45 -0300

    Add contacts

---
+++ EntityFramework6Demo/Data/ContactsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EntityFramework6Demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework6Demo.Data
+{
+    public class ContactsDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public ContactsDbContext(DbContextOptions<ContactsDbContext> options)
+            : base(options)
+        { }
+    }
+}
